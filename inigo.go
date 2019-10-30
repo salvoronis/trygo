80b025f54fa101b2c2cd254d664a7e6d3f44f005
salvoroni <salvoronis@gmail.com>
2019-10-31 01:20:20 +0300

    pzdc

---
+++ inigo.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(res, "Hello, fuck you")
+}
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe("localhost:4000", nil)
+}

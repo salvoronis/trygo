3759f92ff04a73d6e14fcef6397e96a47a12468b
salvoroni <salvoronis@gmail.com>
2019-11-13 00:24:31 +0300

    threads

---
+++ 1bubun/regax_handlers.go
@@ -0,0 +1,65 @@
+package main
+
+import(
+  "fmt"
+  "net/http"
+  "regexp"
+  "strings"
+)
+
+func main (){
+  rr := newPathResolver()
+  rr.Add("GET /hello", hello)
+  rr.Add("(GET|HEAD) /goodbye(/?[A-Za-z0-9]*)?", goodbye)
+  http.ListenAndServe("localhost:8080", rr)
+}
+
+func newPathResolver() *regexResolver {
+  return &regexResolver{
+    handlers: make(map[string]http.HandlerFunc),
+    cache: make(map[string]*regexp.Regexp),
+  }
+}
+
+type regexResolver struct {
+  handlers map[string]http.HandlerFunc
+  cache map[string]*regexp.Regexp
+}
+
+func (r *regexResolver) Add (regex string, handler http.HandlerFunc) {
+  r.handlers[regex] = handler
+  cache, _ := regexp.Compile(regex)
+  r.cache[regex] = cache
+}
+
+func (r *regexResolver) ServeHTTP(res http.ResponseWriter, req *http.Request){
+  check := req.Method + " " + req.URL.Path
+  for pattern, handlerFunc := range r.handlers {
+    if r.cache[pattern].MatchString(check) == true {
+      handlerFunc(res, req)
+      return
+    }
+  }
+}
+
+func hello(res http.ResponseWriter, req *http.Request){
+  query := req.URL.Query()
+  name := query.Get("name")
+  if name == "" {
+    name = "lox"
+  }
+  fmt.Fprint(res, "hello ", name)
+}
+
+func goodbye(res http.ResponseWriter, req *http.Request){
+  path := req.URL.Path
+  parts := strings.Split(path, "/")
+  name := ""
+  if len(parts) > 2 {
+    name = parts[2]
+  }
+  if name == "" {
+    name = "ne lox"
+  }
+  fmt.Fprint(res, "FUCK ", name)
+}
